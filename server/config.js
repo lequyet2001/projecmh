e0318eed90b5d235ee2199d12bed9e22b9f989fb
quyet <quyet.lh198327@sis.hust.edu.vn>
2023-11-03 08:03:13 +0700

    as

--- server/config.js
+++ server/config.js
@@ -3,6 +3,9 @@ module.exports = {
   user: 'lequyet', // better stored in an app setting such as process.env.DB_USER
   password: 'Quyet110213', // better stored in an app setting such as process.env.DB_PASSWORD
   server: 'lequyet.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
+  // user:'sa',
+  // password:'123',
+  // server:'localhost',
   port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
   database: 'damh', // better stored in an app setting such as process.env.DB_NAME
 
